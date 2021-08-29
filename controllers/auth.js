0bd6a10371f6bc7b6430592c0808d1cec90e1196
Sudhanshu Dekate <77233279+xidddekate@users.noreply.github.com>
2021-08-29 17:16:17 +0530

    Removed Test and debug Functions

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,16 +3,6 @@ const _ = require('lodash');
 const {validateSignup, validateLogin} = require('../validator/auth')
 const {User} = require('../models/user');
 
-exports.me = async (req, res) => {
-  const user = await User.findById(req.user._id).select('-password');
-  res.send(user);
-}
-
-exports.userId = async (req, res) => {
-  const user = await User.findById(req.params.userId).select('-password');
-  res.send(user);
-}
-
 exports.signup = async (req, res) => {
   const { error } = validateSignup(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
