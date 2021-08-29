66b95e4750752f2a39fb0cc83d42da9e43c9506d
Sudhanshu Dekate <77233279+xidddekate@users.noreply.github.com>
2021-08-29 17:14:43 +0530

    Removed Testing debug endpoints

--- routes/auth.js
+++ routes/auth.js
@@ -10,17 +10,12 @@ const CRUDController = require('../controllers/crud');
 const isAuth = require('../middleware/auth');
 const isAdmin = require('../middleware/admin');
 
-router.get('/me', [isAuth, isAdmin], authController.me);
-
 router.get('/crud/', [isAuth], CRUDController.getPost(), (req, res) => {
     res.json(res.paginatedResults);
 });
 router.post('/crud/', [isAuth, isAdmin], CRUDController.addPost);
 router.patch('/crud/:id', [isAuth, isAdmin], CRUDController.updatePost);
 router.delete('/crud/:id', [isAuth, isAdmin], CRUDController.deletePost);
-
-router.get('/:userId',  authController.userId);
-
 
 router.post('/signup', authController.signup);
 
